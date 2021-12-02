07b3173522fb3fe275f47f2cf84e29126dafc31b
seb <sebastien.suignard@hotmail.fr>
2021-12-02 15:44:19 +0100

    In progress

--- annoucementTracker.py
+++ annoucementTracker.py
@@ -1,11 +1,14 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 import re
 from typing import NamedTuple
 import smtplib
 from email.message import EmailMessage
 from pycoingecko import CoinGeckoAPI
-import json
+import logging
+import logging.config
+import ccxt
 
 class Registry:
     def __init__(self, filePath):
@@ -34,14 +37,18 @@ class Token:
         self.symbol = symbol
         self.marketCap = 0
         self.exchanges= []
+        
+    def __repr__(self):
+        return "<Token name:%s symbol:%s marketCap:%s exchanges:%s>" % (self.name, self.symbol, self.marketCap, self.exchanges)
 
 def notifyByMail(subject, body):
     msg = EmailMessage()
+    #sebcrypto57
     msg['Subject'] = subject
-    msg['From'] = "seb.crypto@hotmail.com"
+    msg['From'] = "sebcrypto57@gmail.com"
     msg['To'] = "sebastien.suignard@hotmail.fr"
     msg.set_content(body)
-    with smtplib.SMTP("smtp.live.com",587) as s:
+    with smtplib.SMTP("smtp.gmail.com",587) as s:
         s.ehlo()
         s.starttls()
         s.ehlo()
@@ -50,11 +57,11 @@ def notifyByMail(subject, body):
 
 def findToken(title):
     if(title.find('Listing Vote') != -1):
-        print(title)
+        #print(title)
         tokenName = re.search(' \- (.+?) \(', title)
-        print(tokenName)
+        #print(tokenName)
         tokenSymbol = re.search('\((.+?)\)', title)
-        print(tokenSymbol)
+        #print(tokenSymbol)
         if(tokenName and tokenSymbol):
             return Token(tokenName.group(1), tokenSymbol.group(1))
     return None
@@ -64,7 +71,7 @@ def findToken(title):
 # <a href="/article/23970" target="_blank" title="Gate.io Listing Vote #236 - Kadena (KDA), ＄13,000 KDA Giveaway 26381">
 # Supposed to return Kadena KDA
 def getNewListingFromGateIo():
-    link = "https://www.gateio.pro/articlelist/ann/3"
+    link = "https://www.gateio.pro/articlelist/ann/2"
     soup = BeautifulSoup(requests.get(link).text, 'html.parser')
     entries = soup.find_all('div', {'class' : 'entry'})
     newListingTokens = []
@@ -85,22 +92,45 @@ def getTokenInfo(token):
             if(coin["symbol"].casefold() == token.symbol.casefold() and coin["name"].casefold() == token.name.casefold()):
                 coinData = coinGeckoAPI.get_coin_by_id(coin["id"])
                 token.marketCap = coinData["market_data"]["market_cap"]["usd"]
-
+                for ticker in coinData["tickers"]:
+                    token.exchanges.append(ticker["market"]["identifier"])
+                
     except ValueError as ve:
-        print(ve)
+        logger.info(f'getTokenInfo exception caught: %s', ve)
 
-#getNewListingFromGateIo()
-registry = Registry("/home/seb/Workspace/ListingWatcher/gateio_annoucement.txt")
-newTokens = getNewListingFromGateIo()
-for token in newTokens:
-    if(registry.append(token.symbol)):
-        print(token.symbol + " added in registry")
-        getTokenInfo(token)
-        newTokens.append(token)
-        print(token.marketCap)
+def getMyExchanges():
+    return ['gate']
+
+def getExchangeToBuyOn(token):
+    if(token.marketCap > 500000):
+        return None
+    myExchanges = getMyExchanges()
+    for exchange in token.exchanges:
+        if exchange.casefold() in map(str.casefold, myExchanges):
+            return exchange
 
 
-#if(newTokens):
-    #notifyByMail("New currency listing announced on Gate.io", '\n'.join(newTokens))
+if __name__ == '__main__':
+
+    logging.config.fileConfig(fname='log.conf')
+    logger = logging.getLogger('root')
+    logger.debug('This is a debug message')
+
+    registry = Registry(os.getcwd() + '/gateio_annoucement.txt')
+    newTokens = []
+    for token in getNewListingFromGateIo():
+        if(registry.append(token.symbol)):
+            getTokenInfo(token)
+            newTokens.append(token)
+            logger.info(f'New listing found on gateio: %s', token)
+
+    if(newTokens):
+        for token in newTokens:
+            exchange = getExchangeToBuyOn(token)
+            if exchange:
+                logger.info("I should buy here: %s", exchange)
+
+    #if(newTokens):
+        #notifyByMail("New currency listing announced on Gate.io", '\n'.join(newTokens))
 
 
